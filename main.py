2864ddbc599fb4315a1ff865825bef8d9d860446
pawel <pawelsk.vp.pl@gmial.com>
2021-12-10 22:58:12 +0100

    hey i found sth to commit sonow theres 3 commits

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def main():
     tree = DecisionTree(DEPTH)
     tree.fit(attributes, classes, train_data, train_labels, validation_data, validation_labels)
 
+    test_acc = tree.evaluate(test_data, test_labels)
+    print('\nTest accuracy: ', test_acc)
+
 
 if __name__ == "__main__":
     main()
